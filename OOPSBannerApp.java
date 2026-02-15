3cfbb9713007d39ebbbb94e7d90f3e6049bf28cf
Renuvardhan <rk1759@srmist.edu.in>
2026-02-16 00:18:56 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,3 @@
-/**
- * OOPSBannerApp UC2 – Banner Print Statements
- *
- * Displays OOPS in a simple banner format.
- *
- * @author Developer
- * @version 1.1
- */
 
 public class OOPSBannerApp {
 
