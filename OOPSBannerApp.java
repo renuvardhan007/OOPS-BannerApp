39502d49d507358ff1b15e676e5ba23d5cb817b4
renuvardhan007 <rk1759@srmist.edu.in>
2026-02-16 00:07:07 +0530

    UC3: Refactored banner into printBanner() method

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,14 @@
-/**
- * OOPSBannerApp UC2 – Banner Print Statements
- *
- * Displays OOPS in a simple banner format.
- *
- * @author Developer
- * @version 1.1
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+        printBanner();
+    }
 
+    public static void printBanner() {
         System.out.println("OOOO   OOOO  PPPP   SSSS");
         System.out.println("O   O  O   O P   P  S");
         System.out.println("O   O  O   O PPPP   SSSS");
-        System.out.println("O   O  O   O P      S   S");
+        System.out.println("O   O  O   O P          S");
         System.out.println("OOOO   OOOO  P       SSSS");
     }
 }
