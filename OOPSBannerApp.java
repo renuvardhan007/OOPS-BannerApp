14834784141a50393243f682c39f3c72ddd700d4
Renuvardhan <rk1759@srmist.edu.in>
2026-02-16 08:40:13 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,10 +4,10 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("OOOO   OOOO  PPPP   SSSS");
-        System.out.println("O   O  O   O P   P  S");
-        System.out.println("O   O  O   O PPPP   SSSS");
-        System.out.println("O   O  O   O P      S   S");
-        System.out.println("OOOO   OOOO  P       SSSS");
+        System.out.println(" ****    ****   ****    **** ");
+        System.out.println("*    *  *    *  *   *  *    ");
+        System.out.println("*    *  *    *  ****   **** ");
+        System.out.println("*    *  *    *  *          * ");
+        System.out.println(" ****    ****   *      **** ");
     }
 }
