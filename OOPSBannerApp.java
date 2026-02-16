3bcd98d218d995511c9ba0f99c7916a159f6f534
Renuvardhan <rk1759@srmist.edu.in>
2026-02-16 09:38:05 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,11 +2,13 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        System.out.println("OOOO   OOOO  PPPP   SSSS");
-        System.out.println("O   O  O   O P   P  S");
-        System.out.println("O   O  O   O PPPP   SSSS");
-        System.out.println("O   O  O   O P      S   S");
-        System.out.println("OOOO   OOOO  P       SSSS");
+        System.out.println(String.join(" ", " ****** ", " ****** ", " ****** ", " ****** "));
+        System.out.println(String.join(" ", "**    **", "**    **", "**    **", "**      "));
+        System.out.println(String.join(" ", "**    **", "**    **", "**    **", "**      "));
+        System.out.println(String.join(" ", "**    **", "**    **", "******  ", " *****  "));
+        System.out.println(String.join(" ", "**    **", "**    **", "**      ", "      **"));
+        System.out.println(String.join(" ", "**    **", "**    **", "**      ", "      **"));
+        System.out.println(String.join(" ", " ****** ", " ****** ", "**      ", " ****** "));
+    
     }
 }
