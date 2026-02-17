c60934a591e95ab95c41f3ec22fd2b09fdce07d5
renuvardhan007 <rk1759@srmist.edu.in>
2026-02-17 10:50:43 +0530

    UC4: Implement banner using String array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,25 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        printBanner();
-    }
 
-    public static void printBanner() {
-        System.out.println("OOOO   OOOO  PPPP   SSSS");
-        System.out.println("O   O  O   O P   P  S");
-        System.out.println("O   O  O   O PPPP   SSSS");
-        System.out.println("O   O  O   O P          S");
-        System.out.println("OOOO   OOOO  P       SSSS");
+        String[] lines = new String[7];
+
+        lines[0] = String.join("  ","  ***  ","  ***  "," ***** "," ***** ");
+
+        lines[1] = String.join("  "," *   * "," *   * "," *   * "," *     ");
+
+        lines[2] = String.join("  "," *   * "," *   * "," *   * "," *     ");
+
+        lines[3] = String.join("  "," *   * "," *   * "," ***** "," ***** ");
+
+        lines[4] = String.join("  "," *   * "," *   * "," *     ","     * ");
+
+        lines[5] = String.join("  "," *   * "," *   * "," *     ","     * ");
+
+        lines[6] = String.join("  ","  ***  ","  ***  "," *     "," ***** ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
