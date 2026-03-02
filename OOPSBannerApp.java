9330e82253b95ce41ab4f755bc1e4c9b7b14c97e
renuvardhan007 <rk1759@srmist.edu.in>
2026-03-02 10:33:00 +0530

    UC6: Refactored banner using static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,54 @@
 public class OOPSBannerApp {
 
+    public static String[] getOPattern() {
+        return new String[] {
+                "   ******   ",
+                " **      ** ",
+                " **      ** ",
+                " **      ** ",
+                " **      ** ",
+                " **      ** ",
+                "   ******   "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+                " ******  ",
+                " **    ** ",
+                " **    ** ",
+                " ******  ",
+                " **       ",
+                " **       ",
+                " **       "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[] {
+                "  ****** ",
+                " **       ",
+                " **       ",
+                "  ****** ",
+                "       ** ",
+                "       ** ",
+                " ******  "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] lines = {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-            String.join("  ","  ***  ","  ***  "," ***** "," ***** "),
-            String.join("  "," *   * "," *   * "," *   * "," *     "),
-            String.join("  "," *   * "," *   * "," *   * "," *     "),
-            String.join("  "," *   * "," *   * "," ***** "," ***** "),
-            String.join("  "," *   * "," *   * "," *     ","     * "),
-            String.join("  "," *   * "," *   * "," *     ","     * "),
-            String.join("  ","  ***  ","  ***  "," *     "," ***** ")
-        };
-
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(
+                    o[i] + "   " +
+                    o[i] + "   " +
+                    p[i] + "   " +
+                    s[i]
+            );
         }
     }
 }
